a7dd2e7db19531c916c95136078c2ed73f688d7d
Gracy Bagga <bagga2131@saskpolytech.ca>
2025-04-01 23:02:01 -0600

    Changed calendar card on dashboard

--- src/generalDashboardComponents/CalendarCard.jsx
+++ src/generalDashboardComponents/CalendarCard.jsx
@@ -5,14 +5,14 @@ import "./css/CalendarCardDB.css";
 
 const CalendarCard = ({theme}) => {
   return (
-    <div className={`card shadow-sm text-center rounded calendar-container ${theme === "dark" ? "calendar-dark" : "calendar-light"}`}>
+    <div className={`card shadow-sm text-center rounded calendar-container ${theme === "dark" ? "calendar-dark" : "calendar-light"}`} style={{height:'461px'}}>
       <div className="card-body text-center">
         <h5 className={`card-title ${theme==='light'?'text-dark':'text-light'}`}>Calendar</h5>
         <div className="d-flex justify-content-center align-items-center">
           <Calendar value={new Date()} />
         </div>
       </div>
-      <p className={`${theme==='light'?'text-dark':'text-light'}`}>
+      <p className={`${theme==='light'?'text-dark':'text-light'}`} style={{fontSize:'1.5rem'}}>
           <strong>Today's Date:</strong> {new Date().toDateString()}
         </p>
     </div>
